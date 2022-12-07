eca290ae379a144529e6a5ad365589572300ec43
virajpatel16 <virajgondaliya200@gmail.com>
2022-12-07 10:58:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/resume/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.resume;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText
+   edt_name,edt_bob,edt_father,edt_sex,edt_address,edt_email,edt_contact,edt_nation,edt_religion,edt_status,edt_language,edt_hobbies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn =findViewById(R.id.btn);
+        edt_name=findViewById(R.id.edt_name);
+        edt_bob=findViewById(R.id.edt_dob);
+        edt_father=findViewById(R.id.edt_father);
+        edt_sex=findViewById(R.id.edt_sex);
+        edt_address=findViewById(R.id.edt_address);
+        edt_email=findViewById(R.id.edt_email);
+        edt_contact=findViewById(R.id.edt_contact);
+        edt_nation=findViewById(R.id.edt_nation);
+        edt_religion=findViewById(R.id.edt_religion);
+        edt_status=findViewById(R.id.edt_status);
+        edt_language=findViewById(R.id.edt_language);
+        edt_hobbies=findViewById(R.id.edt_hobbies);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=edt_name.getText().toString();
+                String dob=edt_bob.getText().toString();
+                String father=edt_father.getText().toString();
+                String sex=edt_sex.getText().toString();
+                String address=edt_address.getText().toString();
+                String email=edt_email.getText().toString();
+                String contact=edt_contact.getText().toString();
+                String nation=edt_nation.getText().toString();
+                String religion=edt_religion.getText().toString();
+                String status=edt_status.getText().toString();
+                String language=edt_language.getText().toString();
+                String hobbies=edt_hobbies.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this,SecondaryActivity.class);
+                intent.putExtra("name",name);
+                intent.putExtra("dob",dob);
+                intent.putExtra("father",father);
+                intent.putExtra("sex",sex);
+                intent.putExtra("address",address);
+                intent.putExtra("email",email);
+                intent.putExtra("contact",contact);
+                intent.putExtra("nation",nation);
+                intent.putExtra("religion",religion);
+                intent.putExtra("status",status);
+                intent.putExtra("language",language);
+                intent.putExtra("hobbies",hobbies);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/resume/SecondaryActivity.java
@@ -0,0 +1,65 @@
+package com.example.resume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.nio.charset.StandardCharsets;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    TextView name,Dob,father,address,status,hobbies,language,religion,nation,email,contact,sex;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+
+        name=findViewById(R.id.name);
+        Dob=findViewById(R.id.dob);
+        father=findViewById(R.id.father);
+        address=findViewById(R.id.address);
+        status=findViewById(R.id.status);
+        hobbies=findViewById(R.id.hobbies);
+        language=findViewById(R.id.language);
+        religion=findViewById(R.id.religion);
+        nation=findViewById(R.id.nation);
+        email=findViewById(R.id.email);
+        contact=findViewById(R.id.contact);
+        sex=findViewById(R.id.sex);
+
+
+        String Name = getIntent().getStringExtra("name");
+        String dob = getIntent().getStringExtra("dob");
+        String Father = getIntent().getStringExtra("father");
+        String Sex = getIntent().getStringExtra("sex");
+        String Address = getIntent().getStringExtra("address");
+        String Contact = getIntent().getStringExtra("contact");
+        String Nation = getIntent().getStringExtra("nation");
+        String Religion = getIntent().getStringExtra("religion");
+        String Status = getIntent().getStringExtra("status");
+        String Language = getIntent().getStringExtra("language");
+        String Hobbies = getIntent().getStringExtra("hobbies");
+        String Email = getIntent().getStringExtra("email");
+
+
+
+        name.setText(""+Name);
+        Dob.setText(""+Dob);
+        father.setText(""+Father);
+        sex.setText(""+Sex);
+        address.setText(""+Address);
+        contact.setText(""+Contact);
+        nation.setText(""+Nation);
+        email.setText(""+Email);
+        religion.setText(""+Religion);
+        status.setText(""+Status);
+        language.setText(""+Language);
+        hobbies.setText(""+Hobbies);
+
+
+    }
+}
